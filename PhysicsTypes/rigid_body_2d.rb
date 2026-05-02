ed37a6b9e9a54df1e4fd300e81e7fd2d792a8376
FlyMilk1 <j.yontcheff@gmail.com>
2026-05-03 01:23:18 +0300

    Rigidbody implementation

---
+++ PhysicsTypes/rigid_body_2d.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative 'vector2'
+
+class RigidBody2d
+  attr_accessor :mass, :velocity, :acceleration, :angular_velocity, :angular_acceleration, :position, :rotation
+
+  def initialize(mass, position, rotation)
+    @mass = mass
+    @velocity = Vector2.new(0, 0)
+    @acceleration = Vector2.new(0, 0)
+    @angular_velocity = 0.0
+    @angular_acceleration = 0.0
+    @position = position
+    @rotation = rotation
+    @drag = 0.0
+  end
+
+  def update(time_step)
+    @velocity.x += @acceleration.x * time_step
+    @velocity.y += @acceleration.y * time_step
+    @position.x += @velocity.x * time_step
+    @position.y += @velocity.y * time_step
+    @angular_velocity += @angular_acceleration * time_step
+    @rotation += @angular_velocity * time_step
+
+    #deceleration with drag
+    @velocity.multiply_with_scalar 1 - @drag * time_step
+
+  end
+
+  def add_linear_force(force)
+    @acceleration.x += force.x / @mass
+    @acceleration.y += force.y / @mass
+  end
+
+end
---
+++ yordan_physics_2d.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'PhysicsTypes/vector2'
+require_relative 'PhysicsTypes/rigid_body_2d'
+
+module YordanPhysics2d
+end
